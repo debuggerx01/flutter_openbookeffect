479bed01e606d6adf47d565a851f0bd8c23a0489
debuggerx <dx8917312@163.com>
2018-07-12 14:23:13 +0800

    修改依赖

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'package:flutter/scheduler.dart' show timeDilation;
 import 'package:flutter/material.dart';
 import 'package:flutter_openbookeffect/OpenBookEffect.dart';
-import 'package:flutter_openbookeffect/RectGetter.dart';
+import 'package:rect_getter/rect_getter.dart';
 import 'package:flutter_openbookeffect/SV.dart';
 import 'package:flutter_openbookeffect/newPage.dart';
 
@@ -43,11 +43,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       ),
     );
 
-    AnimationController openAniController =
-        new AnimationController(duration: new Duration(milliseconds: 300), lowerBound: 0.0, upperBound: 1.0, vsync: this);
+    AnimationController openAniController = new AnimationController(
+        duration: new Duration(milliseconds: 300), lowerBound: 0.0, upperBound: 1.0, vsync: this);
 
-    AnimationController fillAniController =
-        new AnimationController(duration: new Duration(milliseconds: 100), lowerBound: 0.0, upperBound: 1.0, vsync: this);
+    AnimationController fillAniController = new AnimationController(
+        duration: new Duration(milliseconds: 100), lowerBound: 0.0, upperBound: 1.0, vsync: this);
 
     return new Scaffold(
       appBar: new AppBar(
@@ -61,7 +61,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             sliver: new SliverGrid.count(
               crossAxisSpacing: 10.0,
               crossAxisCount: 3,
-              children: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24].map((i) {
+              children:
+                  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24].map((i) {
                 RectGetter bookCard = new RectGetter.defaultKey(
                   child: new Card(
                     child: new SizedBox.expand(
@@ -95,8 +96,9 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                                 var offset = OpenBookEffect.getOffsetFromCentrePoint(context, rect);
                                 offset *= openAniController.value;
 
-                                double scaleRatio =
-                                    openAniController.value * SV.openBookScaleRadioKey * OpenBookEffect.getMaxScaleRadio(context, rect).minRadio;
+                                double scaleRatio = openAniController.value *
+                                    SV.openBookScaleRadioKey *
+                                    OpenBookEffect.getMaxScaleRadio(context, rect).minRadio;
 
                                 var translateMatrix4 = new Matrix4.identity()..translate(offset.dx, offset.dy);
 
@@ -121,7 +123,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                                       ),
                                     ),
                                     new Transform(
-                                      origin: new Offset(offset.dx - (rect.width * scaleRatio / 2), offset.dy + rect.height / 2),
+                                      origin: new Offset(
+                                          offset.dx - (rect.width * scaleRatio / 2), offset.dy + rect.height / 2),
                                       transform: flipMatrix4,
                                       child: new Transform(
                                         origin: new Offset(rect.width / 2, rect.height / 2),
@@ -136,8 +139,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                           ),
                     );
 
-                    Rect fillRect = OpenBookEffect.getRectOfRectWithMoveToCenterAndScaled(
-                        context, rect, SV.openBookScaleRadioKey * OpenBookEffect.getMaxScaleRadio(context, rect).minRadio);
+                    Rect fillRect = OpenBookEffect.getRectOfRectWithMoveToCenterAndScaled(context, rect,
+                        SV.openBookScaleRadioKey * OpenBookEffect.getMaxScaleRadio(context, rect).minRadio);
 
                     var scaleRadio = OpenBookEffect.getMaxScaleRadio(context, fillRect);
 
@@ -148,7 +151,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                               animation: fillAniController,
                               builder: (BuildContext context, Widget child) {
                                 var fillMatrix4 = new Matrix4.identity()
-                                  ..scale(1 + scaleRadio.w * fillAniController.value, 1 + scaleRadio.h * fillAniController.value);
+                                  ..scale(1 + scaleRadio.w * fillAniController.value,
+                                      1 + scaleRadio.h * fillAniController.value);
 
                                 return new Transform(
                                   origin: new Offset(fillRect.width / 2, fillRect.height / 2),
@@ -170,8 +174,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         SV.index = i;
 
                         //播放填充动画,完成时导航至阅读器页面
-                        Navigator.of(context).push(new PageRouteBuilder(
-                            pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+                        Navigator.of(context).push(new PageRouteBuilder(pageBuilder:
+                            (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
                           //直接返回页面即可避免播放默认路由动画
                           return SV.secPage;
                         }))
